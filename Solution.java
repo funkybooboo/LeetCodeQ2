acbb71ce12b9f5d07b0eda776913df6c49a0e5a8
Nate Stott <nathanstott01@gmail.com>
2023-07-02 16:42:40 -0600

    set up

--- Solution.java
+++ Solution.java
@@ -2,12 +2,26 @@ public class Solution {
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        public ListNode() {
+
+        }
+        public ListNode(int val) {
+            this.val = val;
+        }
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Input: l1 = [2,4,3], l2 = [5,6,4]");
+        System.out.println("Output: [7,0,8]");
+        System.out.println("Explanation: 342 + 465 = 807.");
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
     }
+
 }
